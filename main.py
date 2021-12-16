87ba5cb535a4b54304a268c05f9c4c14e2720ada
Empezeeet <pawelkomateusz@gmail.com>
2021-12-16 17:30:27 +0100

    wow

--- main.py
+++ main.py
@@ -24,8 +24,6 @@ player.jump_duration = 1
 player.jump_height = 0
 player.speed = 0
 
-player_origin = player.world_position
-player_hitInfo = raycast(player_origin,direction=(1, 1, 1), ignore=(player,), distance=5, debug=True)
 
 
 
@@ -33,24 +31,18 @@ player_hitInfo = raycast(player_origin,direction=(1, 1, 1), ignore=(player,), di
 
 
 window.borderless = False
-cursor = Cursor()
-cursor.visible = False
 
 
     
 
 
 
-enemy_testor = Entity(model='sphere', collider='sphere', color=color.rgb(166, 255, 27), scale=(2.5, 2.5, 2.5))
+enemy_testor = Entity(model='sphere', collider='sphere', color=color.rgb(100, 255, 0), scale=(2.5, 2.5, 2.5))
 enemy_testor.position = (10, 2, 10)
 
 
 
 window.title = "TDfns" # wyglada jak losowe znaki ale to bedzie Tower defense
-#audio = Audio("audio/Lo")
-
-#audio.play()
-#audio.volume = 0.5
 window.fullscreen = False
 
 
@@ -58,23 +50,11 @@ window.fullscreen = False
 enemy1 = Bots(10, 2)
 enemy1.spawn(position= (10, 2, 5))
 
+   
 
 
 
-enable_enemyAI = True
-def enemy_walkingAI(): #Ta funkcja jest dodawana do nowego wątku procesora. 
-    while enable_enemyAI:
-        Bots.bot.position = Bots.bot.position - (1, 0, 1)
-        time.sleep(1)
 
-def enable_enemyWalkingAI():
-    try:
-        enemy1_Ai = threading.Thread(target=enemy_walkingAI)
-        enemy1_Ai.start()
-    except:
-        print('ERROR enemy1_AI exception')
-        app.getExitErrorCode()
-    
 
 
 
@@ -89,8 +69,12 @@ def input(key):
         enemy1.rtp()
     if key == 'x':
         app.userExit()
-    if key == 'q':
-        enable_enemyWalkingAI()
+    if key == 'e':
+        print( 'X:' + str(player.position.x))
+        print( 'Y:' + str(player.position.y))
+        print( 'Z:' + str(player.position.z))
+        
+        
         
 
 def update():
--- scripts/bots.py
+++ scripts/bots.py
@@ -18,5 +18,41 @@ class Bots():
         
     def rtp(self):
         Bots.bot.position = (random.randint(0, 10), 5, random.randint(0, 10))
-    
         
+    def start_attacking_tower(self):
+        if Bots.bot != None:
+            #Settings Move direction
+            spawnPoint = self.bot.position
+            spawnPoint_xStatus = None #True = +x | False = -x
+            spawnPoint_zStatus = None #True = +x | False = -x
+                
+            if (spawnPoint.x < 0):
+                spawnPoint_xStatus = False
+            if (spawnPoint.x > 0):
+                spawnPoint_xStatus = True
+            if (spawnPoint.z < 0):
+                spawnPoint_zStatus = False
+            if (spawnPoint.z > 0):
+                spawnPoint_zStatus = True
+                    
+            isInTower = False
+            while not(isInTower):
+                #Checking is enemy on tower
+                if (self.bot.position == (0, 2, 0)):
+                    self.bot.visible = False
+                    self.bot.disable()
+                    isInTower = True
+                
+                if (spawnPoint_xStatus == False) and (spawnPoint_zStatus == False): #X is on (-) & Z is on (-)
+                    while self.bot.position != (0, 2, 0):
+                            self.bot.position += (1, 0, 1)
+                if (spawnPoint_xStatus == True) and (spawnPoint_zStatus == True): #X is on (+) & Z is on (+)
+                    while self.bot.position != (0, 2, 0):
+                            self.bot.position -= (1, 0, 1)
+                if (spawnPoint_xStatus == True) and (spawnPoint_zStatus == False): #X is on (+) & Z is on (-)
+                    while self.bot.position != (0, 2, 0):
+                            self.bot.position += (-1, 0, 1)
+                if (spawnPoint_xStatus == False) and (spawnPoint_zStatus == True): #X is on (-) & Z is on (+)
+                    while self.bot.position != (0, 2, 0):
+                            self.bot.position += (1, 0, -1)
+                time.sleep(1)
